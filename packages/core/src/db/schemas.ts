4652eb650273f780cb77e42c8aaab34e63ce90a0
Viren070 <viren070@protonmail.com>
2025-07-15 19:15:30 +0100

    fix: allow any string for thumbnail

--- packages/core/src/db/schemas.ts
+++ packages/core/src/db/schemas.ts
@@ -555,7 +555,7 @@ const MetaVideoSchema = z
     title: z.string().or(z.null()).optional(),
     name: z.string().or(z.null()).optional(),
     released: z.string().datetime().or(z.null()).optional(),
-    thumbnail: z.string().url().or(z.null()).optional(),
+    thumbnail: z.string().or(z.null()).optional(),
     streams: z.array(StreamSchema).or(z.null()).optional(),
     available: z.boolean().or(z.null()).optional(),
     episode: z.number().or(z.null()).optional(),
