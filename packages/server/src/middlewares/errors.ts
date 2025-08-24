3af18db213f787baca55a5fa5e5293040815a660
Viren070 <viren070@protonmail.com>
2025-08-24 20:11:39 +0100

    fix: add handling for RATE_LIMIT_EXCEEDED errors in middleware

--- packages/server/src/middlewares/errors.ts
+++ packages/server/src/middlewares/errors.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { createLogger, APIError, constants } from '@aiostreams/core';
+import {
+  createLogger,
+  APIError,
+  constants,
+  StremioTransformer,
+} from '@aiostreams/core';
 import { createResponse } from '../utils/responses';
 
 const logger = createLogger('server');
@@ -23,6 +28,28 @@ export const errorMiddleware = (
     error = new APIError(constants.ErrorCode.INTERNAL_SERVER_ERROR);
   } else {
     error = err;
+  }
+
+  if (error.code === constants.ErrorCode.RATE_LIMIT_EXCEEDED) {
+    const stremioResourceRequestRegex =
+      /^\/stremio\/[0-9a-fA-F-]{36}\/[A-Za-z0-9+/=]+\/(stream|meta|addon_catalog|subtitles|catalog)\/[^/]+\/[^/]+(?:\/[^/]+)?\.json\/?$/;
+    const resource = stremioResourceRequestRegex.exec(req.originalUrl);
+    if (resource) {
+      res.json(
+        StremioTransformer.createDynamicError(
+          resource[1] as
+            | 'stream'
+            | 'meta'
+            | 'addon_catalog'
+            | 'subtitles'
+            | 'catalog',
+          {
+            errorDescription: 'Rate Limit Exceeded',
+          }
+        )
+      );
+      return;
+    }
   }
 
   res.status(error.statusCode).json(
